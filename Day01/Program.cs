45fd8b022b7ab66057d4b464b226155dc953746a
Ulf Wallin <ulf.wallin@svitzer.com>
2024-12-01 08:52:35 +0100

    Day One

---
+++ Day01/Program.cs
@@ -0,0 +1,13 @@
+﻿var lines = File.ReadAllLines("input.txt");
+var left =  lines.Select(l => int.Parse(l[..(l.IndexOf(' '))])).Order();
+var right = lines.Select(l => int.Parse(l[(l.LastIndexOf(' '))..])).Order();
+var result = left.Zip(right, (a, b) => Math.Abs(a - b)).Sum();
+
+Console.WriteLine("Part One: " + result);
+
+long sum = 0;
+foreach (var num in left) {
+    sum += num * right.Count(n => num == n);
+}
+
+Console.WriteLine("Part Two: " + sum);

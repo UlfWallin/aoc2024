59e523fef083251ec06b0ed82e9814d7a3db69a3
Ulf Wallin <ulf.wallin@svitzer.com>
2024-12-02 12:35:51 +0100

    Day 2, part two

--- Day02/Program.cs
+++ Day02/Program.cs
@@ -1,24 +1,43 @@
-﻿var result = 0L;
-var lines = File.ReadAllLines("sample.txt");
-var counsSafe = 0;
+﻿var lines = File.ReadAllLines("input.txt");
+var cntSafe = 0;
+var cntSafe2 = 0;
 
-foreach(var line in lines) {
+foreach (var line in lines)
+{
     var levels = line.Split(' ').Select(s => int.Parse(s)).ToArray();
-    var diffs = levels.Zip(levels[1..], (lhs, rhs) => rhs - lhs).ToArray();
-    
-    var outliers = diffs.Any(d => Math.Abs(d) > 3);
-    var allIncr = diffs.All(d => d <= -1);
-    var allDecr = diffs.All(d => d >= 1);
-        
-    if (!outliers && (allIncr || allDecr)) {
-        counsSafe++;
+    if (IsValid(levels))
+    {
+        cntSafe++;
     }
-    else {
+    else
+    {
         // Part two
-        
+        for (var i = 0; i < levels.Length; i++)
+        {
+            var valid = IsValid(levels.Where((e, j) => j != i).ToArray());
+            if (valid)
+            {
+                cntSafe2++;
+                break;
+            }
+        }
     }
 }
 
-result = counsSafe;
+Console.WriteLine("Part One: {0}", cntSafe);
+Console.WriteLine("Part Two: {0}", cntSafe + cntSafe2);
 
-Console.WriteLine(result);
+static bool IsValid(int[] levels)
+{
+    var diffs = levels.Zip(levels[1..], (lhs, rhs) => rhs - lhs).ToArray();
+
+    var outliers = diffs.Any(d => Math.Abs(d) > 3);
+    var allDecr = diffs.All(d => d <= -1);
+    var allIncr = diffs.All(d => d >= 1);
+
+    if (!outliers && (allIncr || allDecr))
+    {
+        return true;
+    }
+    return false;
+}

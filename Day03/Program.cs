0727515c7fabb81b7e557882dbea7248e0228117
Ulf Wallin <ulf.wallin@svitzer.com>
2024-12-03 12:22:41 +0100

    Day 3, part one and two

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -1,40 +1,35 @@
 ﻿using System.Text.RegularExpressions;
 
 var memory = File.ReadAllText("input.txt");
-
-Console.WriteLine("Part One: {0}", Mult(memory));
-
-const string OP_DO = "do()";
-const string OP_DONT = "don't()";
+Console.WriteLine("Part One: {0}", ProcessMemory(memory));
 
 // Part two - Preprocess
-var posdont = 0;
-posdont = memory.IndexOf(OP_DONT);
+const string OP_DO = "do()";
+const string OP_DONT = "don't()";
+var posdont = memory.IndexOf(OP_DONT);
 while (posdont > 0)
 {
-    if (posdont > 0)
-    {
-        var posdo = memory.IndexOf(OP_DO, posdont + 1);
-        memory = memory.Remove(posdont, posdo - posdont);
-    }
+    var posdo = memory.IndexOf(OP_DO, posdont + 1);
+    memory = memory.Remove(posdont, posdo - posdont);
     posdont = memory.IndexOf(OP_DONT);
 }
 
-Console.WriteLine("Part Two: {0}", Mult(memory));
+Console.WriteLine("Part Two: {0}", ProcessMemory(memory));
 
-static long Mult(string memory)
+static long ProcessMemory(string memory)
 {
     var result = 0L;
     var regex = new Regex(@"mul\((\d{1,3}),(\d{1,3})\)");
     var ops = regex.Matches(memory);
     foreach (Match op in ops)
     {
-        var ar = op.Value[4..]; // Skip "mul("
-        var nums = ar[..^1] // Skip last ')'
+        var ar = op.Value[4..^1]; // Skip "mul(" and ")"
+        var opResult = ar    
             .Split(',')
             .Select(int.Parse)
-            .ToArray();
-        result += nums[0] * nums[1];
+            .Aggregate((c, a) => c * a);
+
+        result += opResult;
     }
 
     return result;

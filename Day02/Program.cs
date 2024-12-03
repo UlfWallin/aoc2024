dd7c715dd258cc3412c199a0ee48d311eac11f06
Ulf Wallin <ulf.wallin@svitzer.com>
2024-12-03 08:24:27 +0100

    Day 3, part one and two

--- Day02/Program.cs
+++ Day02/Program.cs
@@ -1,36 +1,10 @@
 ﻿var lines = File.ReadAllLines("input.txt");
 var cntSafe = 0;
-var cntSafe2 = 0;
-
-foreach (var line in lines)
-{
-    var levels = line.Split(' ').Select(s => int.Parse(s)).ToArray();
-    if (IsValid(levels))
-    {
-        cntSafe++;
-    }
-    else
-    {
-        // Part two
-        for (var i = 0; i < levels.Length; i++)
-        {
-            var valid = IsValid(levels.Where((e, j) => j != i).ToArray());
-            if (valid)
-            {
-                cntSafe2++;
-                break;
-            }
-        }
-    }
-}
-
-Console.WriteLine("Part One: {0}", cntSafe);
-Console.WriteLine("Part Two: {0}", cntSafe + cntSafe2);
+var cntSafeish = 0;
 
 static bool IsValid(int[] levels)
 {
     var diffs = levels.Zip(levels[1..], (lhs, rhs) => rhs - lhs).ToArray();
-
     var outliers = diffs.Any(d => Math.Abs(d) > 3);
     var allDecr = diffs.All(d => d <= -1);
     var allIncr = diffs.All(d => d >= 1);
@@ -41,3 +15,28 @@ static bool IsValid(int[] levels)
     }
     return false;
 }
+
+foreach (var line in lines)
+{
+    var levels = line.Split(' ').Select(s => int.Parse(s)).ToArray();
+    if (IsValid(levels))
+    {
+        cntSafe++;
+    }
+    else
+    {
+        // Part two - using brute force
+        for (var i = 0; i < levels.Length; i++)
+        {
+            var valid = IsValid(levels.Where((e, j) => j != i).ToArray());
+            if (valid)
+            {
+                cntSafeish++;
+                break;
+            }
+        }
+    }
+}
+
+Console.WriteLine("Part One: {0}", cntSafe);
+Console.WriteLine("Part Two: {0}", cntSafe + cntSafeish); 
---
+++ Day03/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.RegularExpressions;
+
+var memory = File.ReadAllText("input.txt");
+
+Console.WriteLine("Part One: {0}", Mult(memory));
+
+const string OP_DO = "do()";
+const string OP_DONT = "don't()";
+
+// Part two - Preprocess
+var posdont = 0;
+posdont = memory.IndexOf(OP_DONT);
+while (posdont > 0)
+{
+    if (posdont > 0)
+    {
+        var posdo = memory.IndexOf(OP_DO, posdont + 1);
+        memory = memory.Remove(posdont, posdo - posdont);
+    }
+    posdont = memory.IndexOf(OP_DONT);
+}
+
+Console.WriteLine("Part Two: {0}", Mult(memory));
+
+static long Mult(string memory)
+{
+    var result = 0L;
+    var regex = new Regex(@"mul\((\d{1,3}),(\d{1,3})\)");
+    var ops = regex.Matches(memory);
+    foreach (Match op in ops)
+    {
+        var ar = op.Value[4..]; // Skip "mul("
+        var nums = ar[..^1] // Skip last ')'
+            .Split(',')
+            .Select(int.Parse)
+            .ToArray();
+        result += nums[0] * nums[1];
+    }
+
+    return result;
+}

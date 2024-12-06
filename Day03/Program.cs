afcacc5bd3e30720f7557de8441f5fe2d7336c62
UlfWallin <herr.wallin@gmail.com>
2024-12-06 12:01:58 +0100

    Minor tweaks, day 3

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -6,30 +6,24 @@ Console.WriteLine("Part One: {0}", ProcessMemory(memory));
 // Part two - Preprocess
 const string OP_DO = "do()";
 const string OP_DONT = "don't()";
-var posdont = memory.IndexOf(OP_DONT);
-while (posdont > 0)
+var posDont = memory.IndexOf(OP_DONT);
+while (posDont > 0)
 {
-    var posdo = memory.IndexOf(OP_DO, posdont + 1);
-    memory = memory.Remove(posdont, posdo - posdont);
-    posdont = memory.IndexOf(OP_DONT);
+    var posDo = memory.IndexOf(OP_DO, posDont + 1);
+    memory = memory.Remove(posDont, posDo - posDont);
+    posDont = memory.IndexOf(OP_DONT);
 }
 
 Console.WriteLine("Part Two: {0}", ProcessMemory(memory));
 
 static long ProcessMemory(string memory)
 {
-    var result = 0L;
     var regex = new Regex(@"mul\((\d{1,3}),(\d{1,3})\)");
+    var result = 0L;
     var ops = regex.Matches(memory);
     foreach (Match op in ops)
     {
-        var ar = op.Value[4..^1]; // Skip "mul(" and ")"
-        var opResult = ar    
-            .Split(',')
-            .Select(int.Parse)
-            .Aggregate((c, a) => c * a);
-
-        result += opResult;
+        result += int.Parse(op.Groups[1].Value) * int.Parse(op.Groups[2].Value);
     }
 
     return result;
